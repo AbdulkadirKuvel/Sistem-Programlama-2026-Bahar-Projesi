95fc29755e7849d15e7897ba80378d21719339c3
Abdulkadir <Abdulkadir.kuvel@ogr.sakarya.edu.tr>
2026-05-24 23:09:33 +0300

    changed main.c

--- src/main.c
+++ src/main.c
@@ -1,34 +1,41 @@
 #include <tarsau.h>
 
-
 int main(int argc, char *argv[]) {
     AppConfig config;
     StatusCode status;
 
+    // 1. Konfigürasyonu varsayılan değerlerle ilklendir
     init_default_config(&config);
 
+    // 2. CLI Argümanlarını ayrıştır ve ön doğrulamaları yap
     status = parse_arguments(argc, argv, &config);
     if (status != TARSAU_SUCCESS) {
-        fprintf(stderr, "Hata: Argümanlar ayrıştırılamadı.\n");
+        // Hata mesajları fonksiyonların içinde basıldığı için doğrudan çıkış yapılır
         return EXIT_FAILURE;
     }
+    // fprintf(stdout, "Bilgi: operational mode: %d\n", config.mode);
 
-    fprintf(stdout, "Bilgi: operational mode: %d\n", config.mode);
+    // 3. Çalışma moduna göre ilgili motoru tetikle
+    if (config.mode == MODE_CREATE_ARCHIVE) {
+        // Arşivleme öncesi katı kısıtları (Boyut, Adet, ASCII kontrolü) denetle
+        status = validate_constraints(&config);
+        if (status != TARSAU_SUCCESS) {
+            return EXIT_FAILURE;
+        }
+        
+        // Arşivi oluştur
+        status = execute_archive_creation(&config);
+        if (status != TARSAU_SUCCESS) {
+            return EXIT_FAILURE;
+        }
 
-    // if (config.mode == MODE_CREATE_ARCHIVE) {
-    //     status = validate_constraints(&config);
-    //     if (status == TARSAU_SUCCESS) {
-    //         status = execute_archive_creation(&config);
-    //     }
-    // } else if (config.mode == MODE_EXTRACT_ARCHIVE) {
-    //     status = execute_archive_extraction(&config);
-    // }
+    } else if (config.mode == MODE_EXTRACT_ARCHIVE) {
+        // Arşivi çöz
+        status = execute_archive_extraction(&config);
+        if (status != TARSAU_SUCCESS) {
+            return EXIT_FAILURE;
+        }
+    }
 
-    // if (status != TARSAU_SUCCESS) {
-    //     /* TODO: Hata koduna göre özelleştirilmiş loglama operasyonu */
-    //     return EXIT_FAILURE;
-    // }
-
-    printf("İşlem başarıyla tamamlandı.\n");
     return EXIT_SUCCESS;
 }
